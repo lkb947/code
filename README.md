fbf17f09020f13e41926ab97e66482983e90a01f
kbl <1280017084@qq.com>
2022-06-20 09:00:07 +0800

    删除其他的内容

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# code
-各种项目的集合
-# python 
-是关于python的项目
 # Little Tools
-是关于一个有小工具集合的app
-# java
-关于java的项目
+用express，ajax,jquery mobile 写的，是关于一个有小工具集合的app
--- java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-// import javax.management.monitor.GaugeMonitor;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
